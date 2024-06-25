a6728c00ddfd176a4f655732db04de0c3d0fd4d4
LionStar2328 <162992373+LionStar2328@users.noreply.github.com>
2024-06-25 11:06:26 -0300

    Add files via upload

---
+++ Site_BellaSoft/js/validar.js
@@ -0,0 +1,13 @@
+function logar(){
+    var email = document.getElementById("email").value;
+    
+
+    if(email == "nome"){
+        alert("Olá")
+        location.href = "../html/login.html";
+        
+    }
+}
+
+
+function cadastrar(){}
